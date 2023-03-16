d62c59200c63fec944e9438eb5547ba1b0b10b12
Jeremy Kolker <jeremykolker@Jeremys-MacBook-Pro.local>
2023-03-16 13:26:49 -0400

    incorporating new calendar concept into app, calendar ejs created, working on js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
@@ -14,7 +13,6 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 
 mongoose.connect(mongoURI)
- 
 
 // INDEX PAGE RENDERED
 app.get("/todoit", (req, res) => {
@@ -28,82 +26,73 @@ app.get("/todoit", (req, res) => {
     });
 });
 
-// CREATE TASK ITEM PAGE ROUTE 
+
+
+// NEW TASK ITEM ROUTE \\ 
 app.get("/todoit/new", (req, res) => {
   res.render("new.ejs");
 });
 
+// CALENDAR ROUTE \\
+app.get("/todoit/calendar", (req, res) => {
+  res.render("calendar.ejs");
+})
 
 // EDIT ROUTE \\
 app.get("/todoit/:id/edit", (req, res) => {
-    Todoit.findById(req.params.id).then((todos) => {
-      res.render("edit.ejs", {
-        Todo: todos
-      });
+  Todoit.findById(req.params.id).then((todos) => {
+    res.render("edit.ejs", {
+      Todo: todos
     });
   });
-  
+});
+
 app.put("/todoit/:id", (req, res) => {
-    Todoit.findByIdAndUpdate(
-      req.params.id, // find the Todo item to update by ID
-      {
-        date: req.body.date,
-        time: req.body.time,
-        location: req.body.location,
-        task: req.body.task,
-        completed: req.body.completed === "on" // checkbox value is "on" if checked
-      },
-      { new: true } // return the updated Todo item in the response
-    ).then((updatedTodo) => {
-      res.redirect("/todoit"); // redirect to the index page after update
+  Todoit.findByIdAndUpdate(
+    req.params.id, // find the Todo item to update by ID
+    {
+      date: req.body.date,
+      time: req.body.time,
+      location: req.body.location,
+      task: req.body.task,
+      completed: req.body.completed === "on" // checkbox value is "on" if checked
+    },
+    { new: true } // return the updated Todo item in the response
+  ).then((updatedTodo) => {
+    res.redirect("/todoit"); // redirect to the index page after update
+  });
+});
+
+//  POST ROUTE WITH REDIRECT TO INDEX
+app.post("/todoit", (req, res) => {
+  Todoit.create(req.body)
+    .then(() => {
+      res.redirect("/todoit");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("error");
     });
-  });
-  
-
-
-
-
-  
-  
-
-  // CREATE ROUTE WITH REDIRECT TO INDEX
-  app.post("/todoit", (req, res) => {
-    Todoit.create(req.body)
-      .then(() => {
-        res.redirect("/todoit");
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("error");
-      });
-  });
-      
-      
-      
-    
+});
 
 // DELETE ROUTE \\
 app.delete('/todoit/:id', (req, res) => {
-    Todoit.findByIdAndRemove(req.params.id)
-      .then(() => Todoit.find({})) // update data after deleting item
-      .then((todos) => {
-        res.render('index.ejs', { Todo: todos }) // render updated index page
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('error');
-      });
-  });
-
-
+  Todoit.findByIdAndRemove(req.params.id)
+    .then(() => Todoit.find({})) // update data after deleting item
+    .then((todos) => {
+      res.render('index.ejs', { Todo: todos }) // render updated index page
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('error');
+    });
+});
 
 
 
 app.listen(port, () => {
   console.log("listening");
 });
-
-
 
 
 
