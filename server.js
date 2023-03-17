27bde8e7adc8ef40e46eed7f779aadfce441d09f
Jeremy Kolker <jeremykolker@Jeremys-MacBook-Pro.local>
2023-03-17 16:55:36 -0400

    fixed delete button, now search bar isnt functioning. somehow the js logic is interfering with both functions

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ mongoose.connect(mongoURI)
 
 // INDEX PAGE RENDERED
 app.get("/todoit", (req, res) => {
-  const timestamp = new Date().getTime(); // get current timestamp
   Todoit.find({})
     .then((todos) => {
-      res.render("index.ejs", { Todo: todos, timestamp: timestamp }); // pass timestamp to view
+      res.render("index.ejs", { Todo: todos });
     })
     .catch((err) => {
       console.error(err);
@@ -49,49 +48,6 @@ app.get("/todoit/:id/edit", (req, res) => {
 });
 
 
-
-
-
-// SEARCH ROUTE \\
-app.get("/todoit/search", (req, res) => {
-  const query = req.query.q; // Get search query from request query parameters
-
-  // Use a regular expression to search for matching Todoit documents
-  Todoit.find({ $or: [
-    { task: { $regex: query, $options: "i" } },
-    { location: { $regex: query, $options: "i" } }
-  ]})
-  .then((results) => {
-    res.render("index.ejs", { Todo: results });
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("error");
-  })
-  .then(() => {
-    res.redirect("/todoit");
-  });
-
-});
-
-app.get("/todoit/:id/edit", (req, res) => {
-  Todoit.findById(req.params.id).then((todos) => {
-    res.render("edit.ejs", {
-      Todo: todos
-    });
-  });
-});
-
-app.delete('/todoit/:id/delete/', (req, res) => {
-  Todoit.findByIdAndRemove(req.params.id).then((todos) => {
- res.redirect("/todoit/")
-  }) 
-   
-   
-});
-
-
-
 app.put("/todoit/:id", (req, res) => {
   Todoit.findByIdAndUpdate(
     req.params.id, // find the Todo item to update by ID
@@ -108,9 +64,6 @@ app.put("/todoit/:id", (req, res) => {
   });
 });
 
-
-
-
 //  POST ROUTE WITH REDIRECT TO INDEX
 app.post("/todoit", (req, res) => {
   Todoit.create(req.body)
@@ -123,12 +76,18 @@ app.post("/todoit", (req, res) => {
     });
 });
 
-
-
-
-
-
-
+// DELETE ROUTE \\
+app.delete('/todoit/:id', (req, res) => {
+  Todoit.findByIdAndRemove(req.params.id)
+    .then(() => Todoit.find({})) // update data after deleting item
+    .then((todos) => {
+      res.render('index.ejs', { Todo: todos }) // render updated index page
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('error');
+    });
+});
 
 
 
